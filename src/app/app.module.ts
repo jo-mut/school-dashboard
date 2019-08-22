483c0c46edc26cea47535ba072a829a27ed0272a
jo_ngei <johnngei2@gmail.com>
2019-08-22 14:00:57 +0300

    Initialized school dashboard template with a sidebar navigation, top navigation and a main panel that will show the dashboard and other layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { MainPanelComponent } from './navigation/main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule
